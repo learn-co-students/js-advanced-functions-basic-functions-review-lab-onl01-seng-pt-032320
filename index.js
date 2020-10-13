9a3783eeeb618e0ad5bcab7e75fdf96b043eff74
nabilhayet <nabil4457@yahoo.com>
2020-10-12 23:07:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(day = "roller-skate"){
+ return `This Saturday, I want to ${day}!`
+}
+
+function mondayWork(day ="go to the office"){
+    return `This Monday, I will ${day}.`
+}
+
+function wrapAdjective(value = "*"){
+    return function(mood="special"){
+        return `You are ${value}${mood}${value}!`
+    }
+}
+
+let encouragingPromptFunction = wrapAdjective("%")("a dedicated programmer")
+
+  const Calculator = function(){
+
+}
