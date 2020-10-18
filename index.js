1307d350c7ce465f98a371e4475a9bb0ca3bde91
Shamel Lakin <christlife4u@gmail.com>
2020-10-18 16:54:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return( `This Saturday, I want to ${activity}!`);
+    }
+   saturdayFun(); 
+
+function mondayWork(activity='go to the office') {
+    return(`This Monday, I will ${activity}.`);
+    }
+    mondayWork("work from home");      
+
+function wrapAdjective(style="*") {
+    return function(say="special"){
+        return(`You are ${style}${say}${style}!`)
+    }
+
+}
